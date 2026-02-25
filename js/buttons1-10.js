8846e0221af64550e91b5502391693d1b9a13286
edutechnoturkiye <edutechnoturkiye@gmail.com>
2026-02-25 13:33:20 +0300

    COMMITTT

--- js/buttons1-10.js
+++ js/buttons1-10.js
@@ -1,1 +1,7 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+  const button = document.querySelector('.btn1');
+  
+  button.addEventListener('click', function() {
+    alert('Button clicked!');
+  });
+});

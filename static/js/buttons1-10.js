67c11f78133e0acc3f14cc82ff25bc96fb844277
edutechnoturkiye <edutechnoturkiye@gmail.com>
2026-03-11 15:07:07 +0300

    COMMIT 0000001 f you

--- static/js/buttons1-10.js
+++
@@ -1,6 +0,0 @@
-const btn1 = document.getElementById("navbtn1");
-const btn2 = document.getElementById("navbtn2");
-const btn3 = document.getElementById("navbtn3");
-const btn4 = document.getElementById("navbtn4");
-
-btn1.addEventListener("focusin", () => {btn1.styl})

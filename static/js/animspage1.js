3fae61c5961e57a173686e15ee2006b760b0278c
edutechnoturkiye <edutechnoturkiye@gmail.com>
2026-03-13 10:55:35 +0300

    im hella bored bro

--- static/js/animspage1.js
+++ static/js/animspage1.js
@@ -1,24 +1,11 @@
-window.addEventListener("load", function () {
-  const body = document.body;
-  body.style.opacity = 0;
-
-  let start = null;
-  const duration = 1750;
-
-  function animate(timestamp) {
-    if (!start) start = timestamp;
-    let progress = timestamp - start;
-    let opacity = Math.min(progress / duration, 1);
-    body.style.opacity = opacity;
-
-    if (progress < duration) {
-      requestAnimationFrame(animate);
-    }
-  }
-
-  requestAnimationFrame(animate);
+document.addEventListener('DOMContentLoaded', function() {
+  document.body.style.opacity = '1';
+  document.body.style.scale = '1';
+  document.body.style.transform = 'translateY(0)';
+  setTimeout(function() {
+    document.body.style.overflowY = 'auto';
+  }, 2000);
 });
-
 /* Space Here */
 
 $(function() {

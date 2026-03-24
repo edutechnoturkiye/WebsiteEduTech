5a253b57392448d47c3da8e553b9646d6fb005dc
edutechnoturkiye <edutechnoturkiye@gmail.com>
2026-03-24 13:50:19 +0300

    commit

---
+++ static/js/buttons1-10.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const settings = document.getElementById('settings');
+
+  settings.addEventListener('click', () => {
+    alert('Bu özellik henüz geliştirilme aşamasındadır. Lütfen daha sonra tekrar kontrol edin.');
+  });
+});

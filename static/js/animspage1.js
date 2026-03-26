8dc1ddbafd663c00da442057a2fa6e285591ab57
edutechnoturkiye <edutechnoturkiye@gmail.com>
2026-03-26 12:25:56 +0300

    FAH ninja ninja ninja ninja ninja ninja ninja

--- static/js/animspage1.js
+++ static/js/animspage1.js
@@ -7,28 +7,3 @@ document.addEventListener('DOMContentLoaded', function() {
   }, 2000);
 });
 /* Space Here */
-
-$(function() {
-
-  var $overlay = $('.modal-overlay');
-
-  $('.open').on('click', function() {
-    var modalID = $(this).data('modal');
-    var $modal = $('#' + modalID);
-
-    $overlay.addClass('state-show');
-    $modal.removeClass('state-leave').addClass('state-appear');
-  });
-
-  $('.close').on('click', function() {
-    var $modal = $(this).closest('.modal-frame');
-
-    $overlay.removeClass('state-show');
-    $modal.removeClass('state-appear').addClass('state-leave');
-  });
-
-  $('.modal-frame').on('animationend webkitAnimationEnd oanimationend MSAnimationEnd', function() {
-    $(this).removeClass('state-leave');
-  });
-
-});

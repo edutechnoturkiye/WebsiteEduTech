8199d87742a14e17571ba7333ad960579f3ee0eb
edutechnoturkiye <edutechnoturkiye@gmail.com>
2026-03-26 12:28:03 +0300

    SDA

--- static/js/buttons1-10.js
+++ static/js/buttons1-10.js
@@ -2,4 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const settings = document.getElementById('settings');
 
   const overlay = document.getElementById('modal-overlay');
+
+  settings.addEventListener('click', () => {
+    overlay.classList.add('active');
+  });
 });

dcd482695f89444d68894af269bb8592a7a3e295
edutechnoturkiye <edutechnoturkiye@gmail.com>
2026-03-30 15:13:08 +0300

    bvnd

--- static/js/animspage1.js
+++ static/js/animspage1.js
@@ -5,5 +5,19 @@ document.addEventListener('DOMContentLoaded', function() {
   setTimeout(function() {
     document.body.style.overflowY = 'auto';
   }, 2000);
-});
+
 /* Space Here */
+
+  const settings = document.getElementById('settings');
+  const overlay = document.getElementById('modal-overlay');
+
+  settings.addEventListener('click', () => {
+    overlay.classList.add('active');
+  });
+
+  var $closebtn = document.querySelector('.close');
+  $closebtn.addEventListener('click', function() {
+    const overlay = document.getElementById('modal-overlay');
+    overlay.classList.remove('active');
+  });
+});
--- static/js/buttons1-10.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const settings = document.getElementById('settings');
-
-  const overlay = document.getElementById('modal-overlay');
-
-  settings.addEventListener('click', () => {
-    overlay.classList.add('active');
-  });
-});
